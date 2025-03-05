6f005098008cafc156b31b274df29597806cdd94
Francis Burns <burnsfrancis@icloud.com>
2025-03-05 02:59:07 +0200

    Updated file paths

--- schoolprojects/websitedevelopment/README.md
+++ schoolprojects/websitedevelopment/README.md
@@ -1,10 +1,8 @@
 # [burnsfrancis.github.io](https://burnsfrancis.github.io)
 
-This website is for practice and training purposes.
+This website is for practice and training purposes. The content should not be taken seriously, nor do I own any of the images, videos of music presented.
 
-Course:
-Verkkosivujen kehittäminen R0244-3021
+Course: Website Development (Verkkosivujen kehittäminen) R0244-3021
+School: Laurea University of Applied Sciences
 
-Student:
-Francis Burns
-2208315
+Student: Francis Burns 2208315
---
+++ send-mail.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect the form data
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $comment = htmlspecialchars($_POST['text']);
+
+    // Config
+    $to = "burnsfrancis@icloud.com"; // Change this to your email address
+    $subject = "Mail from El Tio project";
+    $message = "Name: $name\nEmail: $email\n\nComment:\n$comment";
+    $headers = "From: $email";
+
+    // Send the email
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Your feedback has been sent successfully!";
+    } else {
+        echo "There was an error sending your feedback. Please try again later.";
+    }
+}
+?>
